24794303f402aadd1c543bd1bce2435c6ac19498
Owen Jolivet <owen.jolivet@jpb-systeme.com>
2020-12-04 12:50:24 +0100

    Working, missing auth verif and validation + yt url check

--- assets/app.js
+++ assets/app.js
@@ -9,11 +9,7 @@
 
 import 'popper.js';
 import 'bootstrap';
-
+import 'jquery';
 
 import './styles/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit mek in assets/app.js');
--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Group;
 use App\Entity\Trick;
+use App\Entity\Video;
+use App\Form\ImageType;
+use App\Form\TrickFormType;
 use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Image;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class TrickController extends AbstractController
 {
@@ -16,9 +23,15 @@ class TrickController extends AbstractController
      */
     private $trickRepository;
 
-    public function __construct(TrickRepository $trickRepository)
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(TrickRepository $trickRepository, EntityManagerInterface $entityManager)
     {
         $this->trickRepository = $trickRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -42,6 +55,57 @@ class TrickController extends AbstractController
 
         return $this->render('trick/trick.html.twig', [
             'trick' => $trick
+        ]);
+    }
+
+    /**
+     * @Route("/create/trick", name="trick.create")
+     */
+    public function createTrick(Request $request)
+    {
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickFormType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trick = new Trick();
+            $trick->setName($form->get('name')->getData());
+            $trick->setDescription($form->get('description')->getData());
+            $trick->setCategory($form->get('category')->getData());
+            $this->entityManager->persist($trick);
+
+            foreach ($form['images'] as $image){
+                $file = $image->get('path')->getData();
+                if($file){
+                    $newFilename = uniqid().'.'.$file->guessExtension();
+                    try {
+                        $file->move('images/uploads', $newFilename);
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    $newImage = new Image();
+                    $newImage->setPath($newFilename);
+                    $newImage->setDescription($image->get('description')->getData());
+                    $newImage->setTrick($trick);
+                    $this->entityManager->persist($newImage);
+                }
+            }
+
+            foreach ($form['videos'] as $video){
+                $newVideo = new Video();
+                $newVideo->setUrl($video->get('url')->getData());
+                $newVideo->setDescription($video->get('description')->getData());
+                $newVideo->setTrick($trick);
+                $this->entityManager->persist($newVideo);
+            }
+
+            $this->entityManager->flush();
+            dd($trick);
+        }
+
+        return $this->render('trick/createtrick.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
