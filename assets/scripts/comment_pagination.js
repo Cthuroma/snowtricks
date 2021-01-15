acff501874a1a942fa14e196e7a535f6c485f5a6
Owen Jolivet <46999828+Cthuroma@users.noreply.github.com>
2021-01-15 12:16:01 +0100

    Wireframe like (#35)
    * Comment pagination
    
    * Changed descr and group position to replicate wireframe
    
    Co-authored-by: Owen Jolivet <owen.jolivet@jpb-systeme.com>

---
+++ assets/scripts/comment_pagination.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function () {
+
+  $('.pages-ul').each(function(){
+
+    if($(this).data('page') !== 0){
+
+      $(this).css('display', 'none');
+
+    }
+
+  });
+
+  $('.page-item').each(function(){
+
+    $(this).click(function(){
+      page = $(this).data('page');
+      $('.pages-ul').each(function(){
+        if($(this).data('page') === page){
+          $(this).css('display', 'block');
+        }else{
+          $(this).css('display', 'none');
+        }
+      });
+      $('.page-item').each(function() {
+        $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+    });
+  });
+
+});
